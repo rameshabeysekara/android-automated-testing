9c2edcaac7613d0e676209eb5dc2b265c4d4d09d
Ramesh Abeysekara <47961710+rameshabeysekara@users.noreply.github.com>
2024-04-04 12:15:45 -0400

    Add files and sample test cases

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorTest.java
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorTest.java
@@ -14,17 +14,19 @@
  * limitations under the License.
  */
 
+package com.example.android.testing.espresso.BasicSample;
+
+import android.app.Activity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-
-import com.example.android.testing.espresso.BasicSample.MainActivity;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -34,11 +36,12 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+
 /**
  * Basic tests showcasing simple view matchers and actions like {@link ViewMatchers#withId},
  * {@link ViewActions#click} and {@link ViewActions#typeText}.
  * <p>
- * Note that there is no need to tell Espresso that a view is in a different {@link MainActivity}.
+ * Note that there is no need to tell Espresso that a view is in a different {@link Activity}.
  */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -50,8 +53,7 @@ public class ChangeTextBehaviorTest {
      * Use {@link ActivityScenarioRule} to create and launch the activity under test, and close it
      * after test completes. This is a replacement for {@link androidx.test.rule.ActivityTestRule}.
      */
-    @Rule
-    public ActivityScenarioRule<MainActivity> activityScenarioRule
+    @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
---
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityTest.java
@@ -0,0 +1,71 @@
+package com.example.android.testing.espresso.BasicSample;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void validateTextView() {
+        // Validate correct strings in the TextView in the main activity.
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("Hello Espresso!")));
+    }
+
+    @Test
+    public void validateEditText() {
+        // Enter "123" and press Change Text button, and test the string
+        onView(withId(R.id.editTextUserInput)).perform(typeText("123"), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("123")));
+
+        // Enter "123" and press Open Activity and Change Text button, and test the string in ShowTextActivity
+        onView(withId(R.id.editTextUserInput)).perform(typeText("123"), closeSoftKeyboard());
+        onView(withId(R.id.activityChangeTextBtn)).perform(click());
+        onView(withId(R.id.show_text_view)).check(matches(withText("123")));
+
+        // Simulate pressing the back button
+        onView(withId(R.id.show_text_view)).perform(click());
+        onView(withId(R.id.show_text_view)).check(matches(withText("123"))); // Verify the text again after returning from ShowTextActivity
+
+        // Without entering anything and press Change Text button and test the string (empty/null)
+        onView(withId(R.id.editTextUserInput)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("")));
+
+        // Without entering anything and press Open Activity and Change Text button, and test the string in ShowTextActivity (null).
+        onView(withId(R.id.editTextUserInput)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.activityChangeTextBtn)).perform(click());
+        onView(withId(R.id.show_text_view)).check(matches(withText("")));
+
+        // Enter "abcdef" and press Change Text button, and test the string
+        onView(withId(R.id.editTextUserInput)).perform(typeText("abcdef"), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("abcdef")));
+
+        // Enter "abcdef" and press Open Activity and Change Text button, and test the string in ShowTextActivity
+        onView(withId(R.id.editTextUserInput)).perform(typeText("abcdef"), closeSoftKeyboard());
+        onView(withId(R.id.activityChangeTextBtn)).perform(click());
+        onView(withId(R.id.show_text_view)).check(matches(withText("abcdef")));
+    }
+}
