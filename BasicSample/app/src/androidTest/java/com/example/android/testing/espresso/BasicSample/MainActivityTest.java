de2717a7f69c94d84e65aa8fb1075a7017049f50
Ramesh Abeysekara <47961710+rameshabeysekara@users.noreply.github.com>
2024-04-04 14:45:05 -0400

    bug fixing

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityTest.java
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityTest.java
@@ -6,15 +6,18 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -25,6 +28,11 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
+
+    private void clearTextInEditText() {
+        // Clear the text in the EditText
+        onView(withId(R.id.editTextUserInput)).perform(clearText());
+    }
 
     @Test
     public void validateTextView() {
@@ -38,34 +46,28 @@ public class MainActivityTest {
         onView(withId(R.id.editTextUserInput)).perform(typeText("123"), closeSoftKeyboard());
         onView(withId(R.id.changeTextBt)).perform(click());
         onView(withId(R.id.textToBeChanged)).check(matches(withText("123")));
+        clearTextInEditText();
 
         // Enter "123" and press Open Activity and Change Text button, and test the string in ShowTextActivity
         onView(withId(R.id.editTextUserInput)).perform(typeText("123"), closeSoftKeyboard());
         onView(withId(R.id.activityChangeTextBtn)).perform(click());
         onView(withId(R.id.show_text_view)).check(matches(withText("123")));
-
-        // Simulate pressing the back button
-        onView(withId(R.id.show_text_view)).perform(click());
-        onView(withId(R.id.show_text_view)).check(matches(withText("123"))); // Verify the text again after returning from ShowTextActivity
+        onView(isRoot()).perform(ViewActions.pressBack());
+        clearTextInEditText();
 
         // Without entering anything and press Change Text button and test the string (empty/null)
         onView(withId(R.id.editTextUserInput)).perform(typeText(""), closeSoftKeyboard());
         onView(withId(R.id.changeTextBt)).perform(click());
         onView(withId(R.id.textToBeChanged)).check(matches(withText("")));
+        clearTextInEditText();
 
         // Without entering anything and press Open Activity and Change Text button, and test the string in ShowTextActivity (null).
         onView(withId(R.id.editTextUserInput)).perform(typeText(""), closeSoftKeyboard());
         onView(withId(R.id.activityChangeTextBtn)).perform(click());
         onView(withId(R.id.show_text_view)).check(matches(withText("")));
+        onView(isRoot()).perform(ViewActions.pressBack());
+        clearTextInEditText();
 
-        // Enter "abcdef" and press Change Text button, and test the string
-        onView(withId(R.id.editTextUserInput)).perform(typeText("abcdef"), closeSoftKeyboard());
-        onView(withId(R.id.changeTextBt)).perform(click());
-        onView(withId(R.id.textToBeChanged)).check(matches(withText("abcdef")));
-
-        // Enter "abcdef" and press Open Activity and Change Text button, and test the string in ShowTextActivity
-        onView(withId(R.id.editTextUserInput)).perform(typeText("abcdef"), closeSoftKeyboard());
-        onView(withId(R.id.activityChangeTextBtn)).perform(click());
-        onView(withId(R.id.show_text_view)).check(matches(withText("abcdef")));
+        
     }
 }
